118eeb92bd047cfe2a19db0d665fb0b1bcf40460
radz22 <radzsantillan01@gmail.com>
2025-08-10 15:08:30 +0800

    fix: fix responsive

--- components/editor-fast.tsx
+++ components/editor-fast.tsx
@@ -12,7 +12,7 @@ const EditorFast = () => {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 fade-up">
           {[
             {
               badge: "BETTER EDITING",
--- components/works-section.tsx
+++ components/works-section.tsx
@@ -14,7 +14,7 @@ const WorksSection = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 fade-up">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 fade-up">
           {[
             {
               step: "1",
